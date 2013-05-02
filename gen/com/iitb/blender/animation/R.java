6a4538a5221b4621cc74776750cd8cafaf3355bc
ninad chilap <ninadchilap@gmail.com>
2013-05-02 10:05:41 +0530

    added downloading option

--- gen/com/iitb/blender/animation/R.java
+++ gen/com/iitb/blender/animation/R.java
@@ -25,25 +25,30 @@ public final class R {
         public static final int playbutton3=0x7f020006;
     }
     public static final class id {
-        public static final int LinearLayout01=0x7f080006;
-        public static final int help=0x7f080009;
-        public static final int helpwebView=0x7f080003;
-        public static final int imageView1=0x7f080001;
+        public static final int LinearLayout01=0x7f08000a;
+        public static final int btnNo=0x7f080004;
+        public static final int btnyes=0x7f080003;
+        public static final int help=0x7f08000d;
+        public static final int helpwebView=0x7f080007;
+        public static final int imageView1=0x7f080005;
         public static final int listView1=0x7f080000;
-        public static final int lstCourses=0x7f080005;
-        public static final int menu_settings=0x7f08000a;
-        public static final int title=0x7f080002;
-        public static final int videoLayout=0x7f080007;
-        public static final int videoView1=0x7f080008;
-        public static final int webview=0x7f080004;
+        public static final int lstCourses=0x7f080009;
+        public static final int menu_settings=0x7f08000e;
+        public static final int textView1=0x7f080001;
+        public static final int textView2=0x7f080002;
+        public static final int title=0x7f080006;
+        public static final int videoLayout=0x7f08000b;
+        public static final int videoView1=0x7f08000c;
+        public static final int webview=0x7f080008;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int folderrow=0x7f030001;
-        public static final int help=0x7f030002;
-        public static final int htmlcontent=0x7f030003;
-        public static final int main=0x7f030004;
-        public static final int videoview=0x7f030005;
+        public static final int download_source=0x7f030001;
+        public static final int folderrow=0x7f030002;
+        public static final int help=0x7f030003;
+        public static final int htmlcontent=0x7f030004;
+        public static final int main=0x7f030005;
+        public static final int videoview=0x7f030006;
     }
     public static final class menu {
         public static final int activity_main=0x7f070000;
--- src/com/iitb/blender/animation/MainActivity.java
+++ src/com/iitb/blender/animation/MainActivity.java
@@ -1,27 +1,48 @@
 package com.iitb.blender.animation;
 
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.List;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 	String Path;
 	ListView lv;
+	 AlertDialog help_dialog;
+	    private ProgressDialog mProgressDialog, progressBar;
+	    final Context context = this;
 	  int i = 0;
+	  File checkTar;
 	     @Override
   public void onCreate(Bundle savedInstanceState) {
   //	ArrayList<String> CourseList=null;
@@ -30,6 +51,8 @@ public class MainActivity extends Activity {
   
       super.onCreate(savedInstanceState);
       setContentView(R.layout.activity_main);
+      
+  	checkTar = new File("/mnt/sdcard/blenderanimation.zip");
       if (new File("/mnt/extsd/blender animation/").exists()||new File("/mnt/sdcard/blender animation/").exists())
   	{
   			if (new File("/mnt/extsd/blender animation/").exists())
@@ -69,28 +92,249 @@ public class MainActivity extends Activity {
 	});
   	}
       else{
-    	  AlertDialog.Builder builder = new AlertDialog.Builder(this);
-      	builder.setIcon(R.drawable.blender_animation_icon);
-      	builder.setTitle("Blender animations are not present in the tablet!!!");
-      	builder.setMessage(	"Please check whether animations are present at any one of the"+"\n"
-      	+"following locations"+"\n"+"\n"+"1. go to mnt/sdcard/blender animation"
-      			+"\n"+"2. go to mnt/extsd/blender animation")
-      	
-      	       .setCancelable(false)
-      	       .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-      	           public void onClick(DialogInterface dialog, int id) {
-      	        	
-      	        	  MainActivity.this.finish();
-      	        	
-      	           }
-      	       });
-      	AlertDialog alert = builder.create();   
-      	alert.show();
-  	
+    	// download
+      	// extract
+      	// reboot
+      	//Toast.makeText(context, "start downloading", Toast.LENGTH_SHORT).show();
+      	LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+          final View layout = inflater.inflate(R.layout.download_source,null);
+
+          // Building DatepPcker dialog
+          AlertDialog.Builder builder = new AlertDialog.Builder(
+                  MainActivity.this);        	        	
+          builder.setView(layout);
+          builder.setTitle("Notice");
+          builder.setCancelable(false);
+          Button btnNO = (Button) layout.findViewById(R.id.btnNo);
+         
+          btnNO.setOnClickListener(new OnClickListener() {
+          	public void onClick(View v) {
+          		
+          		AlertDialog.Builder builder = new AlertDialog.Builder(context);
+              	builder.setIcon(R.drawable.blender_animation_icon);
+              	builder.setTitle("blender animation videos are not present in the tablet!!!");
+              	builder.setMessage(	"Store the lecture videos at any one of the"+"\n"
+              	+"following locations"+"\n"+"\n"+"1. mnt/sdcard/blender animation"
+              			+"\n"+"2. mnt/extsd/blender animation")
+              	
+              	       .setCancelable(false)
+              	       .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+              	           public void onClick(DialogInterface dialog, int id) {
+              	        	
+              	        	   MainActivity.this.finish();
+              	        	
+              	           }
+              	       });
+              	AlertDialog alert = builder.create();   
+              	alert.show();
+              }	
+          });	
+        
+          Button btnyes = (Button) layout.findViewById(R.id.btnyes);
+          btnyes.setOnClickListener(new OnClickListener() {
+              public void onClick(View v) {
+                  startDownload();
+                  mProgressDialog = new ProgressDialog(context);
+                  mProgressDialog.setMessage("Downloading file..");
+                  mProgressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+                  mProgressDialog.setCancelable(false);
+                  mProgressDialog.setButton(DialogInterface.BUTTON_NEGATIVE,"Cancel",new DialogInterface.OnClickListener() {
+                    
+                      public void onClick(DialogInterface dialog, int which) {
+                          final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                          builder.setMessage("Are you sure you want cancel downloading?")
+                                  .setCancelable(false)
+                                  .setPositiveButton("Yes",
+                                          new DialogInterface.OnClickListener() {
+                                              public void onClick(DialogInterface dialog, int id) {
+                                                  dialog.dismiss();
+                                                  help_dialog.dismiss();
+                                                  finish();
+                                                  android.os.Process.killProcess(android.os.Process.myPid());
+                                              }
+                                          })
+                                  .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                                      public void onClick(DialogInterface dialog, int id) {
+                                          mProgressDialog.show();
+                                      }
+                                  });
+                          AlertDialog alert = builder.create();
+                          alert.show();
+                        
+                      }
+                  });
+                  mProgressDialog.show();
+              }
+          });
+          
+          help_dialog = builder.create();
+          help_dialog.show();
+          WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
+          // customizing the width and location of the dialog on screen
+          lp.copyFrom(help_dialog.getWindow().getAttributes());
+          lp.width = 500;
+          help_dialog.getWindow().setAttributes(lp);
       }
 	     }
-
 	     
+	     private void startDownload() {
+	     	if(isInternetOn()) {
+	             // INTERNET IS AVAILABLE, DO STUFF..
+	                 Toast.makeText(context, "Connected to network", Toast.LENGTH_SHORT).show();
+	             }else{
+	             // NO INTERNET AVAILABLE, DO STUFF..
+	                 Toast.makeText(context, "Network disconnected", Toast.LENGTH_SHORT).show();
+	                 //rebootFlag = 1;
+	                 AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+	                 builder.setMessage("No Connection Found, please check your network setting!")
+	                         .setCancelable(false)
+	                         .setPositiveButton("OK",
+	                                 new DialogInterface.OnClickListener() {
+	                                     public void onClick(DialogInterface dialog, int id) {
+	                                         finish();
+	                                         android.os.Process
+	                                                 .killProcess(android.os.Process.myPid());
+	                                     }
+	                                 });
+	                 AlertDialog alert = builder.create();
+	                 alert.show();
+	               
+	             }  
+	     	/**
+	     	 * global github link for downloading image
+	     	 **/
+	     	String url = "http://www.it.iitb.ac.in/AakashApps/repo/blenderanimation.zip";
+	     	new DownloadFileAsync().execute(url);
+	     }    
+	 	
+	     private final boolean isInternetOn() {
+	     	// check internet connection via wifi   
+	     	 	ConnectivityManager connec =  (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+	     	 	//NetworkInfo mwifi = connec.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+	     	 	//mwifi = connec.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+	             if( connec.getNetworkInfo(0).getState() == NetworkInfo.State.CONNECTED ||
+	             connec.getNetworkInfo(0).getState() == NetworkInfo.State.CONNECTING ||
+	             connec.getNetworkInfo(1).getState() == NetworkInfo.State.CONNECTING ||
+	             connec.getNetworkInfo(1).getState() == NetworkInfo.State.CONNECTED ) {
+	             	//Toast.makeText(this, connectionType + ” connected”, Toast.LENGTH_SHORT).show();
+	             	return true;
+	             } 
+	             else if( connec.getNetworkInfo(0).getState() == NetworkInfo.State.DISCONNECTED ||  
+	             		connec.getNetworkInfo(1).getState() == NetworkInfo.State.DISCONNECTED  ) {
+	             		//System.out.println(“Not Connected”);
+	             		return false;
+	             	}
+	             	return false;
+	     }
+
+	     private void spinner() {
+		    	// will start spinner first and then extraction
+		    	
+		    	// start spinner to show extraction progress
+		    	progressBar = new ProgressDialog(context);
+		        progressBar.setCancelable(false);
+		        progressBar.setMessage("Extracting files, please wait...");
+		        progressBar.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+		        progressBar.show();
+		        String zipFile = Environment.getExternalStorageDirectory() + "/blenderanimation.zip"; 
+		        String unzipLocation = Environment.getExternalStorageDirectory()+"/"; 
+		        new File("mnt/sdcard/blender animation").mkdir();
+		       
+		        
+		        Decompress d = new Decompress(zipFile, unzipLocation); 
+		        d.unzip(); 
+		      //  Toast.makeText(context, "unzipped", Toast.LENGTH_SHORT).show();
+		        
+		       
+		    }
+		
+		 class DownloadFileAsync extends AsyncTask<String, String, String> {
+		    	/**
+		    	 * download zip from URL and write in '/mnt/sdcard'
+		    	 **/
+		        @Override        	
+		        public void onPreExecute() {
+		            super.onPreExecute();
+		        }
+
+		        public String doInBackground(String... aurl) {
+		            int count;
+
+		            try {
+		                URL url = new URL(aurl[0]);
+		                URLConnection conexion = url.openConnection();
+		                conexion.connect();
+
+		                int lenghtOfFile = conexion.getContentLength();
+
+		                InputStream input = new BufferedInputStream(url.openStream());
+		                OutputStream output = new FileOutputStream(
+		                        "/mnt/sdcard/blenderanimation.zip");
+
+		                byte data[] = new byte[1024];
+
+		                long total = 0;
+
+		                while ((count = input.read(data)) != -1) {
+		                    total += count;
+		                    publishProgress("" + (int) ((total * 100) / lenghtOfFile));
+		                    output.write(data, 0, count);
+		                }
+		                output.flush();
+		                output.close();
+		                input.close();
+		            } catch (Exception e) {
+		            }
+		            return null;
+
+		        }
+
+		        public void onProgressUpdate(String... progress) {
+		            mProgressDialog.setProgress(Integer.parseInt(progress[0]));
+		        }
+		        
+		        public void onPostExecute(String unused) {
+		        	mProgressDialog.dismiss();
+		        	help_dialog.dismiss();
+		        	if (checkTar.exists()){
+		        		spinner();
+		        		
+		        	}
+		        	  new Thread() {
+		        		    public void run() {
+		        		        try{
+		        		            // just doing some long operation
+		        		            Thread.sleep(10000);
+		        		         } catch (Exception e) {  }
+		        		           // handle the exception somehow, or do nothing
+		        		         
+
+		        		         // run code on the UI thread
+		        		        runOnUiThread(new Runnable() {
+
+		        		            public void run() {
+		        		                progressBar.dismiss();
+		        		                Intent intent = getIntent();
+		       	        		     finish();
+		       	        		     startActivity(intent);
+		        		            }
+		        		        });
+		        		    }
+		        		     }.start();
+		        		     
+		        		    
+		       // 	progressBar.dismiss();
+		        	
+		    }
+		        //delete internal files during un-installation 
+		        public boolean deleteFile (String name){
+		            name = "aakash.sh";
+		            name = "help_flag";
+		            name = "copyFilesFlag.txt";
+		            return false;
+		           
+		        }
+		}
 
 		 @Override
 		    public boolean onCreateOptionsMenu(Menu menu) {
